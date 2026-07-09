0f7933a2dd0712f444451e80de3ddd562c9c1649
Sachien Ravi <sachien.ravi3217@gmail.com>
2026-07-09 15:28:38 -0500

    Update works page shell layout with missing testimonial slider and cta sections

--- src/pages/WorksPage.tsx
+++ src/pages/WorksPage.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import WorksHeroSection from "../imports/HeroSection-2/index";
+import TestimonialSlider from "../imports/Section-7/index";
+import CTASection from "../imports/Section-13/index";
 import FooterSection from "../imports/Section-17/index";
 
 export default function WorksPage() {
@@ -8,11 +10,24 @@ export default function WorksPage() {
   }, []);
 
   return (
-    <div className="bg-[#141414] min-w-[1440px] overflow-x-hidden">
-      <div className="relative w-full h-[840px]">
+    <div className="bg-[#141414] min-w-[1440px] overflow-x-hidden flex flex-col items-center">
+      {/* 1. Works Hero & Interactive Table List */}
+      <div className="relative w-[1440px] h-[840px] shrink-0">
         <WorksHeroSection />
       </div>
-      <div className="relative w-full h-[640px]">
+
+      {/* 2. Client Testimonials Slider */}
+      <div className="relative w-[1440px] h-[450px] shrink-0">
+        <TestimonialSlider />
+      </div>
+
+      {/* 3. Call to Action (CTA) */}
+      <div className="relative w-[1440px] h-auto shrink-0">
+        <CTASection title="Ready to build your growth engine?" ctaText="Talk with us" />
+      </div>
+
+      {/* 4. Global Footer */}
+      <div className="relative w-[1440px] h-[640px] shrink-0">
         <FooterSection />
       </div>
     </div>
